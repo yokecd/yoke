95e6a864cf81b9bc4ca97f089589f13fc36a7cba
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2024-11-07 07:04:05 -0500

    pkg/openapi: minimalistic kube style openapi generation

---
+++ pkg/openapi/schema_test.go
@@ -0,0 +1,45 @@
+package openapi
+
+import (
+	"reflect"
+	"testing"
+
+	apiext "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenerateSchema(t *testing.T) {
+	type S struct {
+		Name   string            `json:"name"`
+		Age    int               `json:"age"`
+		Labels map[string]string `json:"labels,omitempty"`
+		Active bool              `json:"active"`
+	}
+
+	require.EqualValues(
+		t,
+		&apiext.JSONSchemaProps{
+			Type: "object",
+			Properties: apiext.JSONSchemaDefinitions{
+				"name": apiext.JSONSchemaProps{
+					Type: "string",
+				},
+				"age": apiext.JSONSchemaProps{
+					Type: "integer",
+				},
+				"active": apiext.JSONSchemaProps{
+					Type: "boolean",
+				},
+				"labels": apiext.JSONSchemaProps{
+					Type: "object",
+					AdditionalProperties: &apiext.JSONSchemaPropsOrBool{
+						Schema: &apiext.JSONSchemaProps{Type: "string"},
+					},
+				},
+			},
+			Required: []string{"name", "age", "active"},
+		},
+		GenerateSchema(reflect.TypeOf(S{})),
+	)
+}
