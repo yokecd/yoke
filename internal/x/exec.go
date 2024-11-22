5b7e436a22f3068987606efe54d885b3bd850744
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2024-11-22 14:53:05 -0500

    internal/releaser: release dockerfiles

--- internal/x/exec.go
+++ internal/x/exec.go
@@ -1,6 +1,10 @@
 package x
 
 import (
+<<<<<<< Updated upstream
+=======
+	"fmt"
+>>>>>>> Stashed changes
 	"os"
 	"os/exec"
 	"regexp"
@@ -30,10 +34,16 @@ func Dir(d string) XOpt {
 type XOpt func(*xoptions)
 
 func X(line string, opts ...XOpt) error {
+	return Xf(line, nil, opts...)
+}
+
+func Xf(line string, printArgs []any, opts ...XOpt) error {
 	var options xoptions
 	for _, apply := range opts {
 		apply(&options)
 	}
+
+	line = fmt.Sprintf(line, printArgs...)
 
 	args := regexp.MustCompile(`\s+`).Split(line, -1)
 
