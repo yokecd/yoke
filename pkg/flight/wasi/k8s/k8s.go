fb61c5366f5f8fbc76556e065720022bfc887177
davidmdm <desmarais.michaud.david@gmail.com>
2025-02-05 01:29:13 -0500

    wasi/k8s: validate resource ownership on k8s.Lookup

--- pkg/flight/wasi/k8s/k8s.go
+++ pkg/flight/wasi/k8s/k8s.go
@@ -4,8 +4,10 @@ import (
 	"encoding/json"
 	"errors"
 
+	"github.com/yokecd/yoke/internal"
 	"github.com/yokecd/yoke/internal/wasm"
 
+	"github.com/yokecd/yoke/pkg/flight"
 	// Make sure to include wasi as it contains necessary "malloc" export that will be needed
 	// for the host to allocate a wasm.Buffer. IE: any wasm module that uses this package exports wasi.malloc
 	_ "github.com/yokecd/yoke/pkg/flight/wasi"
@@ -34,10 +36,31 @@ func Lookup[T any](identifier ResourceIdentifier) (*T, error) {
 
 	switch state {
 	case wasm.StateOK:
+		var obj struct {
+			Metadata struct {
+				Labels map[string]string `json:"labels,omitempty"`
+			} `json:"metadata,omitempty"`
+		}
+		if err := json.Unmarshal(buffer.Slice(), &obj); err != nil {
+			return nil, err
+		}
+
+		labels := func() map[string]string {
+			if obj.Metadata.Labels == nil {
+				return map[string]string{}
+			}
+			return obj.Metadata.Labels
+		}()
+
+		if labels[internal.LabelYokeRelease] != flight.Release() || labels[internal.LabelYokeReleaseNS] != flight.Namespace() {
+			return nil, ErrorForbidden("cannot access resource outside of target release ownership")
+		}
+
 		var resource T
 		if err := json.Unmarshal(buffer.Slice(), &resource); err != nil {
 			return nil, err
 		}
+
 		return &resource, nil
 	case wasm.StateError:
 		return nil, errors.New(buffer.String())
