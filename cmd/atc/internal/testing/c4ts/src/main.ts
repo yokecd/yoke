34f0acaa59c537a05ae67aef154b81e669915957
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2025-05-22 09:56:49 -0400

    atc/testing: refactors c4ts server

--- cmd/atc/internal/testing/c4ts/src/main.ts
+++ cmd/atc/internal/testing/c4ts/src/main.ts
@@ -1,38 +1,40 @@
-import * as fs from 'node:fs';
-import * as path from 'node:path';
-import * as http from 'node:http';
+import * as fs from 'node:fs'
+import * as path from 'node:path'
+import * as http from 'node:http'
 
-const images = path.join(__dirname, 'imgs');
+const images = path.join(__dirname, 'imgs')
 
 const cats = fs
   .readdirSync(images, { withFileTypes: true })
   .filter(file => !file.isDirectory())
-  .map(file => fs.readFileSync(path.join(images, file.name)));
+  .map(file => fs.readFileSync(path.join(images, file.name)))
+
+let i = 0
 
 const server = http.createServer((_, resp) => {
-  const cat = cats[Math.floor(100 * Math.random()) % cats.length];
+  const cat = cats[i++ % cats.length]
 
   resp
     .writeHead(200, {
       'Content-Type': 'image/jpeg',
       'Content-Length': cat.length,
     })
-    .end(cat);
-});
+    .end(cat)
+})
 
-const port = Number(process.env['PORT']) || 3000;
+const port = Number(process.env['PORT']) || 3000
 
 server
   .listen(port)
   .on('listening', () => console.log(`listening on port ${port}`))
   .on('error', err => {
-    console.error(`error starting server: ${err.message}`);
-    process.exit(1);
-  });
+    console.error(`error starting server: ${err.message}`)
+    process.exit(1)
+  })
 
 await Promise.any([
   new Promise(resolve => process.on('SIGINT', resolve)),
   new Promise(resolve => process.on('SIGTERM', resolve)),
-]);
+])
 
-server.close(() => process.exit(0));
+server.close(() => process.exit(0))
