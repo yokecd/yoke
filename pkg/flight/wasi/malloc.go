8149767bba3c33b9ca108cd4f407a39610ed525c
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2025-09-21 12:41:23 -0400

    pkg/flight/wasi: capture reference to malloc-ed memory so that GC cannot overwrite returned buffers

--- pkg/flight/wasi/malloc.go
+++ pkg/flight/wasi/malloc.go
@@ -2,9 +2,22 @@
 // It exports a "malloc" func to let hosts allocate memory within the wasm module.
 package wasi
 
-import "github.com/yokecd/yoke/internal/wasm"
+import (
+	"github.com/yokecd/yoke/internal/wasm"
+)
+
+// global serves to capture memory that has been allocated via malloc.
+// This way GC cannot clean up the memory that is being used.
+//
+// TODO: If we want clients to be long lived, we may want to free memory.
+// Host -> mallocs
+// Guest -> Frees
+var global [][]byte
 
 //go:wasmexport malloc
 func malloc(size uint32) wasm.Buffer {
-	return wasm.FromSlice(make([]byte, size))
+	memory := make([]byte, size)
+	global = append(global, memory)
+	buffer := wasm.FromSlice(memory)
+	return buffer
 }
