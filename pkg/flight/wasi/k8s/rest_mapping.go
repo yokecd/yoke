957285b2cc23d6688e404fb6405636758de731ca
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2025-10-15 20:56:27 -0400

    wasi/k8s: fixed panic message for get rest mapping in non wasip1 setting

--- pkg/flight/wasi/k8s/rest_mapping.go
+++ pkg/flight/wasi/k8s/rest_mapping.go
@@ -5,5 +5,5 @@ package k8s
 import "github.com/yokecd/yoke/internal/wasm"
 
 func getRestMapping(ptr wasm.Ptr, groupOrAPIVersion, kind wasm.String) wasm.Buffer {
-	panic("mock lookup not implemented: should be used in the context of wasip1")
+	panic("mock getRestMapping not implemented: should be used in the context of wasip1")
 }
