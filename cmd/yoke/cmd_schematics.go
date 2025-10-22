599546a17452eac77f704918a7a7fb148efa1e66
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2025-10-22 18:03:43 -0400

    pkg/yoke: move schematics cmd logic into public sdk

--- cmd/yoke/cmd_schematics.go
+++ cmd/yoke/cmd_schematics.go
@@ -1,24 +1,14 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	_ "embed"
 	"flag"
 	"fmt"
-	"io"
-	"maps"
-	"net/url"
 	"os"
-	"slices"
 	"strings"
 
-	"k8s.io/apimachinery/pkg/util/yaml"
-
 	"github.com/yokecd/yoke/internal"
-	"github.com/yokecd/yoke/internal/wasi"
-	"github.com/yokecd/yoke/internal/wasi/host"
-	"github.com/yokecd/yoke/internal/wasm/module"
 	"github.com/yokecd/yoke/pkg/yoke"
 )
 
@@ -56,146 +46,56 @@ func SchematicsCommand(ctx context.Context, args []string) error {
 
 	switch subcmd {
 	case "ls":
-		return ListSchematicsCommand(ctx, wasmPath)
+		{
+			schematics, err := yoke.ListSchematics(ctx, yoke.ListSchematicsParams{WasmURL: wasmPath})
+			if err != nil {
+				return fmt.Errorf("failed to list schematics: %w", err)
+			}
+			for _, schematic := range schematics {
+				fmt.Fprintln(internal.Stdout(ctx), schematic)
+			}
+			return nil
+		}
 	case "get":
-		return GetSchematicsCMD(ctx, wasmPath, subargs)
+		{
+			if len(subargs) == 0 || subargs[0] == "" {
+				return fmt.Errorf("name of schematics property is required")
+			}
+			data, err := yoke.GetSchematic(ctx, yoke.GetSchematicParams{
+				WasmURL: wasmPath,
+				Name:    subargs[0],
+			})
+			if err != nil {
+				return fmt.Errorf("failed to get schematics: %w", err)
+			}
+			_, err = internal.Stdout(ctx).Write(data)
+			return err
+		}
 	case "set":
-		return SetSchematicsCMD(ctx, flagset.Usage, wasmPath, subargs)
+		{
+			setcmdFlagset := flag.NewFlagSet("schematics set", flag.ExitOnError)
+			cmd := setcmdFlagset.Bool("cmd", false, "marks the input as command args to be executed to generate the schematic data")
+
+			setcmdFlagset.Usage = func() {
+				flagset.Usage()
+				setcmdFlagset.PrintDefaults()
+			}
+
+			_ = setcmdFlagset.Parse(subargs)
+
+			name := setcmdFlagset.Arg(0)
+			if name == "" {
+				return fmt.Errorf("name of schematics property is required")
+			}
+
+			return yoke.SetSchematic(ctx, yoke.SetSchematicParams{
+				WasmPath: wasmPath,
+				Name:     name,
+				Input:    os.Stdin,
+				CMD:      *cmd,
+			})
+		}
 	default:
 		return fmt.Errorf("unknown schematics subcommand: %q", subcmd)
 	}
-}
-
-func GetSchematicsCMD(ctx context.Context, wasmURL string, args []string) error {
-	wasm, err := yoke.LoadWasm(ctx, wasmURL, false)
-	if err != nil {
-		return fmt.Errorf("failed to load wasm: %w", err)
-	}
-
-	if err := module.ValidatePreamble(wasm); err != nil {
-		return fmt.Errorf("invalid wasm module: failed to validate preamble: %w", err)
-	}
-
-	if len(args) == 0 || args[0] == "" {
-		return fmt.Errorf("name of schematics property is required")
-	}
-
-	name := args[0]
-
-	for key, data := range module.GetCustomSections(wasm) {
-		prefix, key, ok := module.CutSchematicsPrefix(key)
-		if !ok || key != name {
-			continue
-		}
-		switch prefix {
-		case module.PrefixSchematics:
-			_, _ = internal.Stdout(ctx).Write(data)
-		case module.PrefixSchematicsCMD:
-			var args []string
-			if err := yaml.NewYAMLToJSONDecoder(bytes.NewReader(data)).Decode(&args); err != nil {
-				return fmt.Errorf("failed to decode schematic args: %w", err)
-			}
-			output, err := wasi.Execute(ctx, wasi.ExecParams{
-				BinName:       "schematics",
-				Args:          args,
-				CompileParams: wasi.CompileParams{Wasm: wasm, HostFunctionMap: host.BuildFunctionMap(nil)},
-			})
-			if err != nil {
-				return fmt.Errorf("failed to execute schematics: %s: %w", key, err)
-			}
-			_, _ = internal.Stdout(ctx).Write(output)
-		}
-		return nil
-	}
-
-	return fmt.Errorf("schematics property not found: %s", name)
-}
-
-func ListSchematicsCommand(ctx context.Context, wasmURL string) error {
-	wasm, err := yoke.LoadWasm(ctx, wasmURL, false)
-	if err != nil {
-		return fmt.Errorf("failed to load wasm: %w", err)
-	}
-
-	if err := module.ValidatePreamble(wasm); err != nil {
-		return fmt.Errorf("invalid wasm module: failed to validate preamble: %w", err)
-	}
-
-	for _, key := range slices.Sorted(maps.Keys(module.GetCustomSections(wasm))) {
-		fmt.Fprintln(internal.Stdout(ctx), module.StripSchematicsPrefix(key))
-	}
-	return nil
-}
-
-func SetSchematicsCMD(ctx context.Context, usage func(), wasmPath string, args []string) error {
-	flagset := flag.NewFlagSet("schematics set", flag.ExitOnError)
-	cmd := flagset.Bool("cmd", false, "marks the input as command args to be executed to generate the schematic data")
-	flagset.Usage = func() {
-		usage()
-		flagset.PrintDefaults()
-	}
-	flagset.Parse(args)
-
-	name := flagset.Arg(0)
-	if name == "" {
-		return fmt.Errorf("name of schematics property is required")
-	}
-
-	wasmURL, err := url.Parse(wasmPath)
-	if err != nil {
-		return fmt.Errorf("invalid wasmPath: %w", err)
-	}
-	if wasmURL.Scheme != "" && wasmURL.Scheme != "file" {
-		return fmt.Errorf("failed to set schematics: cannot set schematics on remote wasm asset")
-	}
-
-	wasm, err := yoke.LoadWasm(ctx, wasmPath, false)
-	if err != nil {
-		return fmt.Errorf("failed to load wasm: %w", err)
-	}
-
-	if err := module.ValidatePreamble(wasm); err != nil {
-		return fmt.Errorf("invalid wasm module: failed to validate preamble: %w", err)
-	}
-
-	data, err := io.ReadAll(os.Stdin)
-	if err != nil {
-		return fmt.Errorf("failed to read stdin: %w", err)
-	}
-	if len(data) == 0 {
-		return fmt.Errorf("no data for section")
-	}
-
-	temp, err := os.CreateTemp("", "module.wasm")
-	if err != nil {
-		return fmt.Errorf("failed to create temporary file: %w", err)
-	}
-
-	modified, err := func() ([]byte, error) {
-		if *cmd {
-			var cmdArgs []string
-			if err := yaml.NewYAMLToJSONDecoder(bytes.NewReader(data)).Decode(&cmdArgs); err != nil {
-				return nil, fmt.Errorf("could not parse stdin as command args: %w", err)
-			}
-			return module.WithCustomSectionCMD(wasm, name, cmdArgs), nil
-		}
-		return module.WithCustomSectionData(wasm, name, data), nil
-	}()
-	if err != nil {
-		return err
-	}
-
-	if _, err := temp.Write(modified); err != nil {
-		return fmt.Errorf("failed to write to temporary location: %w", err)
-	}
-
-	if err := temp.Close(); err != nil {
-		return fmt.Errorf("failed to close temporary file: %w", err)
-	}
-
-	if err := os.Rename(temp.Name(), wasmPath); err != nil {
-		return fmt.Errorf("faoled to rename temporary file: %w", err)
-	}
-
-	return nil
 }
