5bbca27a4a8bcea8eb203e9d5c0ed52f9ab01fa3
Riccardo Menegazzo <52837869+riccardomenegazzo@users.noreply.github.com>
2026-09-01 16:24:17 +0200

    yoke/takeoff: add reproducible flight preflight
    Signed-off-by: Riccardo Menegazzo <52837869+riccardomenegazzo@users.noreply.github.com>

---
+++ cmd/yoke/internal/testing/flights/nondeterministic/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"encoding/json"
+	"os"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/yokecd/yoke/pkg/flight"
+)
+
+func main() {
+	nonce := make([]byte, 8)
+	if _, err := rand.Read(nonce); err != nil {
+		panic(err)
+	}
+
+	if err := json.NewEncoder(os.Stdout).Encode(corev1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "ConfigMap",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: flight.Release(),
+		},
+		Data: map[string]string{
+			"nonce": hex.EncodeToString(nonce),
+		},
+	}); err != nil {
+		panic(err)
+	}
+}
